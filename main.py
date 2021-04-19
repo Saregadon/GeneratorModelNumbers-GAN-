0335be77cdbed9e202bb0cc37dcb3a5a80e884fc
Christian S <Saregadon@gmail.com>
2021-04-19 17:13:18 -0500

    keras fix

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-import keras
-import tensorflow as tf
+#if need keras >= 2.2
+#python3.7 -m pip install keras==2.1.5
 
+import numpy as np
+import keras
+import matplotlib
+import tensorflow as tf
+from matplotlib import pyplot
+from keras.datasets.mnist import load_data
+
+(trainX, trainy), (testX, testy) = load_data()
+
+print('Train', trainX.shape, trainy.shape)
+print('Test', testX.shape, testy.shape)
+
+print(tf.__version__)
