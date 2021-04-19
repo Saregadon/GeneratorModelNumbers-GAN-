488c838deb813d90d0c76f10216936694f4d82b3
Christian S <Saregadon@gmail.com>
2021-04-19 17:19:24 -0500

    showing 5x5 of drawn dataset

--- main.py
+++ main.py
@@ -8,9 +8,23 @@ import tensorflow as tf
 from matplotlib import pyplot
 from keras.datasets.mnist import load_data
 
-(trainX, trainy), (testX, testy) = load_data()
 
-print('Train', trainX.shape, trainy.shape)
-print('Test', testX.shape, testy.shape)
+(trainX, trainy), (testX, testy) = load_data()
+#define training data set
+
+print('Train', trainX.shape, trainy.shape) #shows training amount
+print('Test', testX.shape, testy.shape) #shows testing amount
+
+for i in range(25):
+    pyplot.imshow(trainX[i], cmap='gray')
+    pyplot.subplot(5, 5, 1+i)
+    pyplot.axis('off')
+    pyplot.imshow(trainX[i], cmap='gray_r')
+pyplot.show()
+
+X = expand_dims(trainX, axis =-1)
+
+X = X.astype('float32') #changing unsigned to float
+X = X / 255.0 #normalizing data from [0, 255] to [0, 1]
 
 print(tf.__version__)
